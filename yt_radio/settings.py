c3d788707efc1be706cc945d7d766f377ab7f185
TheEmik <theemik@wp.pl>
2022-01-29 09:50:27 +0100

    add host

--- yt_radio/settings.py
+++ yt_radio/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'frontend/build/static'
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = BASE_DIR / 'static/'
 
 
 # Default primary key field type
